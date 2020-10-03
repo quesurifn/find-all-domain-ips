1f25aef53bbc36e2939a255b829c06aed17dd8d8
Kyle Fahey <kyle.c.r.fahey@gmail.com>
2020-10-03 08:36:32 -0500

    Latest

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def worker(i):
 
 if __name__ == '__main__':
     jobs = []
-    for i in range(6):
+    for i in range(20):
         p = multiprocessing.Process(target=worker, args=(i,))
         jobs.append(p)
         p.start()
