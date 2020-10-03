e6fbe9ad6da2e5c49fb1f769980732582e1c0798
Kyle Fahey <kyle.c.r.fahey@gmail.com>
2020-10-03 07:50:06 -0500

    Latest

---
+++ main.py
@@ -0,0 +1,28 @@
+import multiprocessing
+import fileinput
+import socket
+
+socket.setdefaulttimeout(2)
+
+def worker(i):
+    i+= 1
+    with open(f"./parsed_data/{i}", "a") as save_file:
+        for line in fileinput.input(f"./data/{i}"):
+            domain = '.'.join( list( reversed( line.split('\t')[1].split('.') ) ) )
+
+            try:
+                ip_list = socket.gethostbyname(domain)
+                save_file.write(f"{domain},{ip_list}\n")
+            except Exception as e:
+                pass
+
+
+        save_file.close()
+
+
+if __name__ == '__main__':
+    jobs = []
+    for i in range(6):
+        p = multiprocessing.Process(target=worker, args=(i,))
+        jobs.append(p)
+        p.start()
