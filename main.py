25e95300597f34ab8bf0a3b18ee78e144e698fd4
Kyle Fahey <kyle.c.r.fahey@gmail.com>
2020-10-04 18:32:24 -0500

    Close file

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ from datetime import datetime
 socket.setdefaulttimeout(2)
 
 def worker(i):
-    with open(f"./parsed_data/{i}", "a+") as save_file:
-        filename = (str(i).zfill(len(str(111))))
-        buffer_array = []
-        for line in fileinput.input(f"./data/{filename}"):
-            domain = '.'.join( list( reversed( line.split('\t')[1].split('.') ) ) )
-            try:
-                ip_list = socket.gethostbyname(domain)
-                buffer_array.append(ip_list)
-                if(len(buffer_array) == 1000):
+    filename = (str(i).zfill(len(str(111))))
+    buffer_array = []
+    for line in fileinput.input(f"./data/{filename}"):
+        domain = '.'.join( list( reversed( line.split('\t')[1].split('.') ) ) )
+        try:
+            ip_list = socket.gethostbyname(domain)
+            buffer_array.append(ip_list)
+            if(len(buffer_array) == 1000):
+                with open(f"./parsed_data/{i}", "a+") as save_file:
                     save_file.write("\n".join(buffer_array))
                     buffer_array = []
-                    print (f"{datetime.now()} -- WRITING: worker {i}")
+                    print(f"{datetime.now()} -- WRITING: worker {i}")
+                    save_file.close()
             except Exception as e:
                 pass
 
         save_file.write("\n".join(buffer_array))
-        save_file.close()
 
 
 if __name__ == '__main__':
